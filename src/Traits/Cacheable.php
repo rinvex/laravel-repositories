a88d8a51121f4a17afb52047b21cb95cedd75d46
Abdelrahman Omran <me@omranic.com>
2017-04-17 00:43:23 +0200

    Remove file header docblock

--- src/Traits/Cacheable.php
+++ src/Traits/Cacheable.php
@@ -1,18 +1,5 @@
 <?php
 
-/*
- * NOTICE OF LICENSE
- *
- * Part of the Rinvex Repository Package.
- *
- * This source file is subject to The MIT License (MIT)
- * that is bundled with this package in the LICENSE file.
- *
- * Package: Rinvex Repository Package
- * License: The MIT License (MIT)
- * Link:    https://rinvex.com
- */
-
 declare(strict_types=1);
 
 namespace Rinvex\Repository\Traits;
